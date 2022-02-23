9850e1ee1e4b85ef9b0a2ac693becbe6ea2feae7
neko_the_shadow <h.nakamura0903@gmail.com>
2022-02-23 19:37:38 +0900

    10.6.1 Githubの認可

---
+++ ch10_06/go.mod
@@ -0,0 +1,12 @@
+module github.com/nekoTheShadow/real-world-http/ch10_06
+
+go 1.17
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ ch10_06/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/skratchdot/open-golang/open"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+)
+
+//go:embed client_info.json
+var accessTokenBytes []byte
+
+type ClientInfo struct {
+	ClientID     string `json:"ClientID"`
+	ClientSecret string `json:"ClientSecret"`
+}
+
+func main() {
+	var clientInfo ClientInfo
+	if err := json.Unmarshal(accessTokenBytes, &clientInfo); err != nil {
+		panic(err)
+	}
+
+	conf := &oauth2.Config{
+		ClientID:     clientInfo.ClientID,
+		ClientSecret: clientInfo.ClientSecret,
+		Scopes:       []string{"user:email", "gist"},
+		Endpoint:     github.Endpoint,
+	}
+	var token *oauth2.Token
+
+	file, err := os.Open("access_token.json")
+	if os.IsNotExist(err) {
+		url := conf.AuthCodeURL("your state", oauth2.AccessTypeOnline)
+		code := make(chan string)
+		var server *http.Server
+		server = &http.Server{
+			Addr: ":18888",
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "text/html")
+				io.WriteString(w, "<html><script>widow.open('about:blank', '_self').close()</scipt></html>")
+				w.(http.Flusher).Flush()
+
+				code <- r.URL.Query().Get("code")
+				server.Shutdown(context.Background())
+			}),
+		}
+		go server.ListenAndServe()
+		open.Start(url)
+		token, err := conf.Exchange(context.Background(), <-code)
+		if err != nil {
+			panic(err)
+		}
+		file, err := os.Create("access_token.json")
+		if err != nil {
+			panic(err)
+		}
+		json.NewEncoder(file).Encode(token)
+	} else if err == nil {
+		token = &oauth2.Token{}
+		json.NewDecoder(file).Decode(token)
+	} else {
+		panic(err)
+	}
+	client := oauth2.NewClient(context.Background(), conf.TokenSource(context.Background(), token))
+	doSomething(client)
+}
+
+func doSomething(client *http.Client) {
+
+}
